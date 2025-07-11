e434631740e8efa44cce5f4879c46ebadacc78c5
Yuta Sato <y1129s310@icloud.com>
2025-07-11 23:03:33 +0900

    article_likeのvalidationのテストを実装

---
+++ spec/models/article_like_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe ArticleLike, type: :model do
+  describe 'バリデーション' do
+    it '有効な属性値で有効であること' do
+      article_like = build(:article_like)
+      expect(article_like).to be_valid
+    end
+
+    it 'userが必須であること' do
+      article_like = build(:article_like, user: nil)
+      expect(article_like).not_to be_valid
+    end
+
+    it 'articleが必須であること' do
+      article_like = build(:article_like, article: nil)
+      expect(article_like).not_to be_valid
+    end
+
+    it '同じユーザーが同じ記事に複数いいねできないこと' do
+      article = create(:article)
+      user = create(:user)
+      create(:article_like, article: article, user: user)
+      article_like = build(:article_like, article: article, user: user)
+      expect(article_like).not_to be_valid
+    end
+  end
+end
