ddd1d0b512e77b70a20197a63a106a3dc17653fd
Yuta Sato <y1129s310@icloud.com>
2025-07-12 14:12:36 +0900

    確定させたいところだけrubocopを実行

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,16 +9,16 @@
 #  article_id :bigint
 #  user_id    :bigint
 #
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  describe 'バリデーション' do
-    it '有効な属性値で有効であること' do
+  describe "バリデーション" do
+    it "有効な属性値で有効であること" do
       comment = build(:comment)
       expect(comment).to be_valid
     end
 
-    it '本文が必須であること' do
+    it "本文が必須であること" do
       comment = build(:comment, body: nil)
       expect(comment).not_to be_valid
     end
