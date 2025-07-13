c5cc041be2cb3ac3c25186f32b3ce919b278988d
Yuta Sato <y1129s310@icloud.com>
2025-07-13 20:03:36 +0900

    記事一覧を返す API 及び テストを実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,8 @@
-class Api::V1::ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-    # render json: @articles
+module Api::V1
+  class ArticlesController < BaseApiController # base_api_controller を継承
+    def index
+      articles = Article.order(updated_at: :desc)
+      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+    end
   end
 end
