55569358efbe7478027bd069c478f44fd78ffb77
Yuta Sato <y1129s310@icloud.com>
2025-07-25 20:01:50 +0900

    概要 devise_token_auth のメソッドをオーバーライドしよう。 (name カラムを既存メソッドに付与したいため) 新規登録時にレスポンスとして返される header 情報の設定をしよう。

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for "User", at: "auth"
-      as :user do
-        # Define routes for User within this block.
-      end
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
       resources :articles
+      # as :user do
+      #   # Define routes for User within this block.
+      # end
     end
   end
 end
